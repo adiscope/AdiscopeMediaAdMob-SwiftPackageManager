90e61a06eabc83d7549e79bb6a567f034da508c5
kyungboshim <kyungbo.shim@neowiz.com>
2026-03-27 11:51:22 +0900

    5.0.4 배포

--- Package.swift
+++ Package.swift
@@ -12,21 +12,45 @@ let package = Package(
             targets: ["AdiscopeMediaAdMobTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "12.14.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "12.11.0"),
+        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager.git", exact: "7.5.2"),
+        .package(url: "https://github.com/Mintegral-official/MintegralAdSDK-Swift-Package.git", exact: "7.7.9"),
+        .package(url: "https://github.com/bytedance/AdsGlobalPackage.git", exact: "7.4.1-release.1"),
     ],
     targets: [
         .target(
             name: "AdiscopeMediaAdMobTarget",
             dependencies: [
                 .target(name: "AdiscopeMediaAdMob"),
+                .target(name: "LiftoffMonetizeAdapter"),
+                .target(name: "MintegralAdapter"),
+                .target(name: "PangleAdapter"),
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                .product(name: "VungleAdsSDK", package: "VungleAdsSDK-SwiftPackageManager"),
+                .product(name: "MintegralAdSDK", package: "MintegralAdSDK-Swift-Package"),
+                .product(name: "AdsGlobalPackage", package: "AdsGlobalPackage"),
             ],
             path: "Sources"
         ),
         .binaryTarget(
             name: "AdiscopeMediaAdMob",
-            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.2.0/AdiscopeMediaAdMob.zip",
-            checksum: "57e19dbefeb556497d4364f9b5798bfdd3b31b09c1be1b14a129bf753cff650f"
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.0.4/AdiscopeMediaAdMob.zip",
+            checksum: "3dfa28873d09685fa192cec55f8c3850036212b87e8b6ade491508091ae1d211"
+        ),
+        .binaryTarget(
+            name: "LiftoffMonetizeAdapter",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.0.0/LiftoffMonetizeAdapter.xcframework.zip",
+            checksum: "22e5bbffbb74ba3db90d281ce137c42d7487a53442903c2751ab755936132d03"
+        ),
+        .binaryTarget(
+            name: "MintegralAdapter",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.0.0/MintegralAdapter.xcframework.zip",
+            checksum: "4b71bb4b580900b9123c0ca354472374004d5343f7caf071336d5c3d655543d8"
+        ),
+        .binaryTarget(
+            name: "PangleAdapter",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.0.0/PangleAdapter.xcframework.zip",
+            checksum: "f643f912f13524a1dd898263e7b1ee3d3362a6395ca39ef056855497cc7b8ad8"
         ),
     ]
 )
