c025d2ae12f532673500fb3730f15014d1bda9d0
kyungboshim <kyungbo.shim@neowiz.com>
2026-06-25 09:54:43 +0900

    5.4.0 배포

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["AdiscopeMediaAdMobTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "12.14.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.2.0"),
     ],
     targets: [
         .target(
@@ -25,8 +25,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AdiscopeMediaAdMob",
-            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.2.4/AdiscopeMediaAdMob.zip",
-            checksum: "5c3ff0b388df38578c78fffc796bc611beeefb69fea30d0ae0ca955a91c41109"
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.4.0/AdiscopeMediaAdMob.zip",
+            checksum: "b8e3072eb69c5ecfeabb2c7a4bf38311ee126d55839dfd17f1b6f0aaf3506bf3"
         ),
     ]
 )
